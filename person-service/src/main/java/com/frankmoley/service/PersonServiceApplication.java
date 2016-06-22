273568ee8c376b7ab2c2e67c5ff9ed166d068db4
Frank Moley <frank.moley@garmin.com>
2016-06-22 11:13:12 -0500

    Discovery server completion Registration of Person Service with Discovery server

--- person-service/src/main/java/com/frankmoley/service/PersonServiceApplication.java
+++ person-service/src/main/java/com/frankmoley/service/PersonServiceApplication.java
@@ -3,9 +3,11 @@ package com.frankmoley.service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableConfigurationProperties
+@EnableEurekaClient
 public class PersonServiceApplication {
 
 	public static void main(String[] args) {
