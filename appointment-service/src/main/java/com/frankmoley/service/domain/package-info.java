47852f3fbb48a4d9150b5f4a8df75cb48b76e278
Frank Moley <frank.moley@garmin.com>
2016-06-23 17:23:09 -0500

    Removing packages not being used

--- appointment-service/src/main/java/com/frankmoley/service/domain/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Domain classes for the web service for Appointment
- */
-package com.frankmoley.service.domain;
--- appointment-service/src/main/java/com/frankmoley/service/service/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Business services for the Appointment Domain
- */
-package com.frankmoley.service.service;
