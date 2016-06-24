72077d675692f515212bea930ec81b045390cc16
Frank Moley <frank.moley@garmin.com>
2016-06-24 11:57:14 -0500

    Completing Step 3

--- discovery-server/src/main/java/com/frankmoley/util/discovery/DiscoveryServerApplication.java
+++ discovery-server/src/main/java/com/frankmoley/util/discovery/DiscoveryServerApplication.java
@@ -2,8 +2,12 @@ package com.frankmoley.util.discovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
+@EnableConfigurationProperties
 public class DiscoveryServerApplication {
 
 	public static void main(String[] args) {
--- person-service/src/main/java/com/frankmoley/service/PersonServiceApplication.java
+++ person-service/src/main/java/com/frankmoley/service/PersonServiceApplication.java
@@ -3,9 +3,11 @@ package com.frankmoley.service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableConfigurationProperties
+@EnableDiscoveryClient
 public class PersonServiceApplication {
 
 	public static void main(String[] args) {
